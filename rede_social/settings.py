131117f30e5e8ccb9e0285bba0dec9c4c86b1960
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-03-25 12:42:03 -0300

    3 Commit

--- rede_social/settings.py
+++ rede_social/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'usuarios',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Messages
+
+from django.contrib.messages import constants
+
+MESSAGE_TAGS = {
+ constants.DEBUG: 'alert-primary',
+ constants.ERROR: 'alert-danger',
+ constants.SUCCESS: 'alert-success',
+ constants.INFO: 'alert-info',
+ constants.WARNING: 'alert-warning',
+}
