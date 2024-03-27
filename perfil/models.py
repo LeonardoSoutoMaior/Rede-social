2ad8a77a2e8d64e247fb26875043f3a493bf16a2
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-03-27 16:32:04 -0300

    Criação das models para seguidor

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Seguir(models.Model):
+    seguidor = models.ForeignKey(User, related_name='seguidores', on_delete=models.CASCADE)
+    seguindo = models.ForeignKey(User, related_name='seguindo', on_delete=models.CASCADE)
+    data_seguimento = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'{self.seguidor.username} segue {self.seguindo.username}'
+    
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, related_name='perfil', on_delete=models.CASCADE)
+    total_seguidores = models.PositiveIntegerField(default=0)
+    
+    def __str__(self):
+        return f'Perfil de {self.usuario.username}'
