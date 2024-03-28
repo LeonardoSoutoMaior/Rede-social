dc0b7774901629412939264d23218c8015b06aac
Leonardo Souto <leo.soutomaior@hotmail.com>
2024-03-27 22:32:12 -0300

    criação da view para renderizar a pagina meu perfil

--- perfil/urls.py
+++ perfil/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('seguir/<int:perfil_id>/', views.seguir_usuario, name='seguir_usuario'),
+    path('perfil/', views.renderizar_perfil, name="renderizar_perfil"),
+    
     
 ]
--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Perfil, Seguir
+from home.models import Publicacao
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 
-# Create your views here.
+
+def renderizar_perfil(request):
+    publicacoes = Publicacao.objects.filter(autor=request.user)
+    
+    seguindo = Seguir.objects.filter(seguidor=request.user).count()
+    
+    seguidores = Seguir.objects.filter(seguindo=request.user).count()
+    
+    return render(request, 'perfil.html', {'publicacoes': publicacoes,
+                  'seguindo': seguindo,
+                  'seguidores': seguidores})
+
+
+@login_required
+def seguir_usuario(request, usuario_id):
+    perfil_a_seguir = get_object_or_404(Perfil, id=usuario_id)
+    perfil_usuario = request.user.perfil
+    
+    seguindo = Seguir.objects.filter(seguidor=perfil_usuario, seguindo=perfil_a_seguir).exists()
+    
+    if seguindo:
+        Seguir.objects.filter(seguidor=perfil_usuario, seguindo=perfil_a_seguir).delete()
+        seguido = False
+    else:
+        Seguir.objects.create(seguidor=perfil_usuario, seguindo=perfil_a_seguir)
+        seguido = True
+        
+    return JsonResponse({'seguindo': seguido})
